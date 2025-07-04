53150a385d29eb5ec348c04c8ab429b0b2f01c86
[Richard Yiga] <[ry.dev.256@gmail.com]>
2025-07-04 11:28:24 +0300

    m

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -55,6 +55,7 @@ export async function fetchCardData() {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
+
     const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
     const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
     const invoiceStatusPromise = sql`SELECT
@@ -84,6 +85,7 @@ export async function fetchCardData() {
     throw new Error('Failed to fetch card data.');
   }
 }
+
 
 const ITEMS_PER_PAGE = 6;
 export async function fetchFilteredInvoices(
